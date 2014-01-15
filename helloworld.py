a0b8f307789fb962bc384926ddfc6e2dfb4fdf9d
josieh <hollingsworth.josie@gmail.com>
2014-01-15 13:29:50 -0500

    helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+#learning python commands
+
+print "Hello World"
+
+print "my name is Josie" 
+ 
+print "lets count somethings"
+
+print "Boys", 25 + 30 / 6
+print "Girls", 100 - 25 * 3 % 4
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+
+
